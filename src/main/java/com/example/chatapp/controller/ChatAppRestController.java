776ea2b9d4f0fe564d72f5320fbf29b010c5b6ca
Patrik <horvath.patrik001@gmail.com>
2017-05-19 10:25:42 +0200

    commit

--- src/main/java/com/example/chatapp/controller/ChatAppRestController.java
+++ src/main/java/com/example/chatapp/controller/ChatAppRestController.java
@@ -5,9 +5,12 @@ import com.example.chatapp.model.Status;
 import com.example.chatapp.service.MessagesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ChatAppRestController {
@@ -25,4 +28,19 @@ public class ChatAppRestController {
     status.setStatus("ok");
     return status;
   }
+
+  String url = "https://chat-p2p.herokuapp.com/api/message/receive";
+  RestTemplate restTemplate = new RestTemplate();
+
+  @GetMapping("/add")
+  public JsonReceived index(
+          @RequestParam(name = "title") String title,
+          @RequestParam(name = "href") String href) {
+
+    JsonReceived p = new JsonReceived(title, href);
+
+    JsonReceived newPost = restTemplate.postForObject(url, p, JsonReceived.class);
+    return newPost;
+  }
+
 }
