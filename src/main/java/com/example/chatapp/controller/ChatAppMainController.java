582fb303ceb50743987ce02f01c84b544ef2cfb4
Patrik <horvath.patrik001@gmail.com>
2017-05-22 13:47:43 +0200

    commit

--- src/main/java/com/example/chatapp/controller/ChatAppMainController.java
+++ src/main/java/com/example/chatapp/controller/ChatAppMainController.java
@@ -79,7 +79,7 @@ public class ChatAppMainController {
     return "redirect:/";
   }
 
-  String url = "https://peertopeerchatapp.herokuapp.com/api/message/receive";
+  String url = "https://greenfox-chat-app.herokuapp.com/api/message/receive";
 
   @PostMapping(value = "/send")
   public String addMessage(String messages) {
