b75429e9fd8ee5ee4b562e12ec25bc43af6dfa7a
Patrik <horvath.patrik001@gmail.com>
2017-05-25 12:34:04 +0200

    change user

--- src/main/java/com/example/chatapp/controller/ChatAppMainController.java
+++ src/main/java/com/example/chatapp/controller/ChatAppMainController.java
@@ -77,7 +77,7 @@ public class ChatAppMainController {
     return "redirect:/";
   }
 
-  String url = "https://infinite-escarpment-68973.herokuapp.com/api/message/receive";
+  String url = "https://reka-greenfox-p2pchatapp.herokuapp.com/api/message/receive";
  // String url = "https://lit-caverns-63725.herokuapp.com/api/message/receive";
 
   @PostMapping(value = "/send")
